49d33a0b1645203ed2ac2269960ae85c32c0b78d
maxmetcalf12 <maxmetcalf12@gmail.com>
2020-02-28 15:42:16 -0700

    Added initial amount

--- app/controllers/money_calculator_controller.rb
+++ app/controllers/money_calculator_controller.rb
@@ -2,6 +2,7 @@ class MoneyCalculatorController < ApplicationController
   def calculate
     current_age = params[:current_age].to_i
     rate = params[:rate].to_i
+    initial_deposit = params[:initial_deposit].to_i
     monthly_amount = params[:monthly_amount].to_i
     yearly_amount = params[:yearly_amount].to_i
     contribution_stop_age = params[:contribution_stop_age].to_i
@@ -9,7 +10,8 @@ class MoneyCalculatorController < ApplicationController
 
     contribution_years = contribution_stop_age - current_age
     compounding_years = cash_out_age - current_age
-    @total_money = 0
+    rate = 1.0 + rate / 100.0
+    @total_money = initial_deposit * (rate**compounding_years)
 
     (1..compounding_years).each do |year_number|
       compounding_multiple = (1.0 + rate / 100.0)**(compounding_years - year_number)
